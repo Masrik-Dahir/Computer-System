eed0b9b5950baaeb54626554a3e921f42a3e5188
Masrik Dahir <dahirma@vcu.edu>
2021-11-03 23:01:32 -0400

    error fixed!

---
+++ cmake-build-debug/Project_2/CMakeFiles/Project_2.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/masrikdahir/CLionProjects/Computer-System/Project_2/cmsc257-f21-p2.c" "/Users/masrikdahir/CLionProjects/Computer-System/cmake-build-debug/Project_2/CMakeFiles/Project_2.dir/cmsc257-f21-p2.c.o"
+  "/Users/masrikdahir/CLionProjects/Computer-System/Project_2/p2-support.c" "/Users/masrikdahir/CLionProjects/Computer-System/cmake-build-debug/Project_2/CMakeFiles/Project_2.dir/p2-support.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/Project_2/CMakeFiles/Project_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_2.dir/cmsc257-f21-p2.c.o"
+  "CMakeFiles/Project_2.dir/p2-support.c.o"
+  "Project_2"
+  "Project_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
