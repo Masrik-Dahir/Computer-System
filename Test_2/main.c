54171076f642a01eac946604f28866ffd194a7ae
Masrik Dahir <dahirma@vcu.edu>
2021-11-15 16:25:56 -0500

    contents

--- Test_2/main.c
+++ Test_2/main.c
@@ -7,23 +7,31 @@
 # include "stdlib.h"
 
 int main(){
-    int n;
-    printf("Enter size of array\n");
 
+//    Allocated memory size
+    int n;
+
+    printf("Enter size of array\n");
     scanf("%d",&n);
+
 
 //    Allocating memory
     int* A = (int*) malloc(n*sizeof(int));
     for (int index = 0; index < n; index ++){
         A[index] = index + 1;
     }
+//    new Allocated memory size
+    int m = 2*n;
+
 
 //    Reallocating memory
+    int* B = (int*) realloc(A,m*sizeof(int));
+
 
 
 //    Printing the allocated memory
-    for (int index = 0; index < n; index ++){
-        printf("%d ",A[index]);
+    for (int index = 0; index < m; index ++){
+        printf("%d ",B[index]);
     }
 
 
